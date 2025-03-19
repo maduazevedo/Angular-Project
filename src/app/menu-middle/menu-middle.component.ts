ca6bd06630ac645ca37f5b3f168352cbb9f88265
Eduarda Azevedo <dudafazv@gmail.com>
2025-03-19 15:41:06 -0300

    fear: trabalhando com rotas

--- src/app/menu-middle/menu-middle.component.ts
+++ src/app/menu-middle/menu-middle.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-middle',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-middle.component.html',
   styleUrl: './menu-middle.component.css'
 })
--- src/app/menu-top/menu-top.component.ts
+++ src/app/menu-top/menu-top.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-top',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-top.component.html',
   styleUrl: './menu-top.component.css'
 })
