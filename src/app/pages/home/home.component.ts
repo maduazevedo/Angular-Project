d12c5db5423747d08d36a36f3b751a3f2486c8c0
Eduarda Azevedo <dudafazv@gmail.com>
2025-03-19 16:11:34 -0300

    .

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuTopComponent } from "../../menu-top/menu-top.component";
 import { BigTitleComponent } from "../../big-title/big-title.component";
 import { MenuMiddleComponent } from "../../menu-middle/menu-middle.component";
 import { BigCardComponent } from "../../big-card/big-card.component";
@@ -7,7 +6,7 @@ import { SmallCardComponent } from "../../small-card/small-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTopComponent, BigTitleComponent, MenuMiddleComponent, BigCardComponent, SmallCardComponent],
+  imports: [ BigTitleComponent, MenuMiddleComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
